9b86eaf520eec1e58fd524ec4fb0449614e11424
Ramyaa <90338717+RamyaaMalaikani@users.noreply.github.com>
2024-06-13 16:19:52 +0530

    Updates in convertor.h file

--- converters.h
+++ converters.h
@@ -1,1 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
 
+bool TryConvertToInt(const char* Input_str, int* value);
+bool TryConvertToDouble(const char* Input_str, double* value);
+void TryConvertDataFromCSVtoXML(FILE* stream);
+char** SplitString(const char* Input_str, char delimiter);
